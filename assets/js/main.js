2afa076ed1a5f372e139c7b40b53f7befbca93b6
alimahmudlu <alimahmudlu@gmail.com>
2021-06-08 12:39:19 +0400

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+/*
+ *    Version: 1.0.1
+ *     Author: Ali Mahmudlu (alimahmudlu)
+ *    Website: http://alimahmudlu.github.io
+ *     Github: https://github.com/alimahmudlu/
+ *       Repo: https://github.com/alimahmudlu/xin.gov.az/
+ *  Telephone: +994708550235
+ *      Email: alimahmudlu@gmail.com
+*/
+
 function popperFn() {
 	if ($("[data-toggle=popover]").length) {
 		$("[data-toggle=popover]").popover({
@@ -27,9 +37,15 @@ function popperFn() {
 
 	}
 }
-
+function heightControl(mainSection, changeSection) {
+	var mainHeight = $(mainSection).actual('outerHeight');
+	$(changeSection).css('height', mainHeight)
+}
 popperFn()
-
+heightControl('.section-about', '.section-rating')
+$(window).resize(function (){
+	heightControl('.section-about', '.section-rating')
+})
 $(document).on('click', '.section-about-body--video', function () {
 	var videoUrl = $(this).attr('data-video-url');
 
@@ -44,13 +60,4 @@ $(document).on('click', '.section-about-body--video', function () {
 
 	$(this).find('.section-about-body--video-preview').toggle()
 	$(this).find('.section-about-body--video-tag').append(videoTag).toggle()
-})
-
-function heightControl(mainSection, changeSection) {
-	var mainHeight = $(mainSection).actual('outerHeight');
-	$(changeSection).css('height', mainHeight)
-}
-heightControl('.section-about', '.section-rating')
-$(window).resize(function (){
-	heightControl('.section-about', '.section-rating')
 })
